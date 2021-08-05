99d67641a3fe165b39465d7ab754044f958bfb50
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-05 16:45:30 -0400

    js being completed

---
+++ js/site.js
@@ -0,0 +1,24 @@
+// Get the values from the Page
+// starts or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //we call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+
+    //we call displayNumbers
+}   
+
+// Generate numbers from startValue to the endValue
+// logic function(s)
+function generateNumbers(startValue, endValue) {
+
+}
+
+// Display the numbers and mark even numbers bold
+// display or view functions
+function displayNumbers(){
+
+}
